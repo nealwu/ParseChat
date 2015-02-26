7ec2d02e3d25d6e1c09c25011b1f8e50a5c39e68
Neal Wu <nwu@box.com>
2015-02-25 21:23:44 -0800

    Initial Commit

---
+++ ParseChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ParseChat
+//
+//  Created by Neal Wu on 2/25/15.
+//  Copyright (c) 2015 Neal Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ParseChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ParseChat
+//
+//  Created by Neal Wu on 2/25/15.
+//  Copyright (c) 2015 Neal Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
